f84347453e6eae73a57161e5b7f23ae2b48a240e
xianyu110 <1002569303@qq.com>
2023-02-28 20:47:39 +0800

    demo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pkg.plugin.host import EventContext, PluginHost
 
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
+@register(name="Hello", description="hello world", version="0.1", author="Maynor")
 class HelloPlugin(Plugin):
 
     # 插件加载时触发
@@ -30,20 +30,20 @@ class HelloPlugin(Plugin):
             # 阻止该事件默认行为（向接口获取回复）
             event.prevent_default()
 
-    # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+    # # 当收到群消息时触发
+    # @on(GroupNormalMessageReceived)
+    # def group_normal_message_received(self, event: EventContext, **kwargs):
+    #     msg = kwargs['text_message']
+    #     if msg == "hello":  # 如果消息为hello
+    #
+    #         # 输出调试信息
+    #         logging.debug("hello, {}".format(kwargs['sender_id']))
+    #
+    #         # 回复消息 "hello, everyone!"
+    #         event.add_return("reply", ["hello, everyone!"])
+    #
+    #         # 阻止该事件默认行为（向接口获取回复）
+    #         event.prevent_default()
 
     # 插件卸载时触发
     def __del__(self):
